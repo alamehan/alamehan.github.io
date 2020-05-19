5d29352dc8fbc4d86afb9137c1d926d7d474e410
alamehan <raifalam98@gmail.com>
2020-05-19 22:03:11 +0700

    ADD NiceVizual-01

---
+++ projects/nice-vizual/16personalities/script-treemap.js
@@ -0,0 +1,55 @@
+Plotly.d3.csv('https://raw.githubusercontent.com/alamehan/visualization-sunburst-hobi/master/dataset/personalitiestreemap.csv', function (err, rows) {
+  function unpack(rows, key) {
+    return rows.map(function (row) {
+      return row[key]
+    });
+  }
+
+  var data = [{
+    type: "treemap",
+    maxdepth: 2,
+    pathbar: {
+      visible: true,
+      edgeshape: "/",
+    },
+
+    ids: unpack(rows, 'ids'),
+    text: unpack(rows, 'text'),
+    labels: unpack(rows, 'labels'),
+    parents: unpack(rows, 'parents'),
+
+    textposition: "middle center",
+    hoverinfo: "none", //bukan labels, tapi label, lihat dokumentasi
+    hovertext: "",
+
+    insidetextfont: {
+      color: '#FFFFFF'
+    }
+  }];
+
+  var layout = {
+    autosize: true,
+    // width: 650,
+    // height: 650,
+    margin: {
+      l: 0,
+      r: 0,
+      b: 0,
+      t: 32
+    },
+    font: {
+      family: 'Roboto Condensed, sans-serif',
+      size: 21,
+    },
+    colorway: ["#636efa", "#EF553B", "#00cc96", "#ab63fa", "#19d3f3",
+      "#e763fa", "#FECB52", "#FFA15A", "#FF6692", "#B6E880"
+    ],
+    paper_bgcolor: '#FFFFFF',
+  }
+
+  var config = {
+    responsive: true
+  }
+
+  Plotly.newPlot('myDiv', data, layout, config);
+})
