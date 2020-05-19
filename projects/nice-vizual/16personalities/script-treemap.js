323b79df738388a370a381dd1bf55691a58ed760
alamehan <raifalam98@gmail.com>
2020-05-19 22:29:24 +0700

    UPDATE NV-01

--- projects/nice-vizual/16personalities/script-treemap.js
+++ projects/nice-vizual/16personalities/script-treemap.js
@@ -29,17 +29,17 @@ Plotly.d3.csv('https://raw.githubusercontent.com/alamehan/visualization-sunburst
 
   var layout = {
     autosize: true,
-    width: 720,
-    height: 1020,
+    width: 920,
+    height: 1120,
     margin: {
       l: 0,
       r: 0,
       b: 0,
-      t: 32
+      t: 56
     },
     font: {
       family: 'Roboto Condensed, sans-serif',
-      size: 32,
+      size: 46,
     },
     colorway: ["#636efa", "#EF553B", "#00cc96", "#ab63fa", "#19d3f3",
       "#e763fa", "#FECB52", "#FFA15A", "#FF6692", "#B6E880"
