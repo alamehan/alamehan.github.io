04e2539d484feeecac8e8cd5777e276a0312b331
Alamehan <raifalam98@gmail.com>
2020-07-09 12:31:19 +0700

    UPDATE Core NiceLink

--- links/script.js
+++ links/script.js
@@ -8,84 +8,56 @@ $(function () {
 
 $('.alert-show-niceref').click(function () {
   Swal.fire({
-    padding: 0,
-    heightAuto: true,
-    customClass: 'tinggi-modal',
+    backdrop: `rgba(13, 13, 13, 0.92)`,
     background: 'url(assets/img-project-niceref.png) center no-repeat',
-    confirmButtonText: '<a href="assets/img-project-onprogress.png">Open ➜</a>',
-
-    backdrop: `
-        rgba(13, 13, 13, 0.92)
-      `,
+    confirmButtonText: '<a href="#">🔒 Upcoming</a>',
   });
 });
 
 $('.alert-show-nicelist').click(function () {
   Swal.fire({
-    padding: 0,
-    heightAuto: true,
-    customClass: 'tinggi-modal',
+    backdrop: `rgba(13, 13, 13, 0.92)`,
     background: 'url(assets/img-project-nicelist.png) center no-repeat',
-    confirmButtonText: '<a href="https://github.com/alamehan/nice-list">Open ➜</a>',
-
-    backdrop: `
-        rgba(13, 13, 13, 0.92)
-      `,
+    confirmButtonText: '<a href="https://github.com/alamehan/nice-list">Open</a>',
+    showCancelButton: true,
+    cancelButtonText: 'Cancel',
   });
 });
 
 $('.alert-show-nicevizual').click(function () {
   Swal.fire({
-    padding: 0,
-    heightAuto: true,
-    customClass: 'tinggi-modal',
+    backdrop: `rgba(13, 13, 13, 0.92)`,
     background: 'url(assets/img-project-nicevizual.png) center no-repeat',
-    confirmButtonText: '<a href="https://nicevizual.github.io/">Open ➜</a>',
-
-    backdrop: `
-        rgba(13, 13, 13, 0.92)
-      `,
+    confirmButtonText: '<a href="https://nicevizual.github.io/">Open</a>',
+    showCancelButton: true,
+    cancelButtonText: 'Cancel',
   });
 });
 
 $('.alert-show-righttr4ck').click(function () {
   Swal.fire({
-    padding: 0,
-    heightAuto: true,
-    customClass: 'tinggi-modal',
+    backdrop: `rgba(13, 13, 13, 0.92)`,
     background: 'url(assets/img-project-righttr4ck.png) center no-repeat',
-    confirmButtonText: '<a href="assets/image-righttr4ck.pdf">Open ➜</a>',
-
-    backdrop: `
-        rgba(13, 13, 13, 0.92)
-      `,
+    confirmButtonText: '<a href="assets/image-righttr4ck.pdf">Open</a>',
+    showCancelButton: true,
+    cancelButtonText: 'Cancel',
   });
 });
 
 $('.alert-show-atozmind').click(function () {
   Swal.fire({
-    padding: 0,
-    heightAuto: true,
-    customClass: 'tinggi-modal',
+    backdrop: `rgba(13, 13, 13, 0.92)`,
     background: 'url(assets/img-project-atozmind.png) center no-repeat',
-    confirmButtonText: '<a href="https://bit.ly/atozmind">Open ➜</a>',
-
-    backdrop: `
-        rgba(13, 13, 13, 0.92)
-      `,
+    confirmButtonText: '<a href="https://bit.ly/atozmind">Open</a>',
+    showCancelButton: true,
+    cancelButtonText: 'Cancel',
   });
 });
 
 $('.alert-show-knowyourself101').click(function () {
   Swal.fire({
-    padding: 0,
-    heightAuto: true,
-    customClass: 'tinggi-modal',
+    backdrop: `rgba(13, 13, 13, 0.92)`,
     background: 'url(assets/img-project-knowyourself101.png) center no-repeat',
-    confirmButtonText: '<a href="assets/img-project-onprogress.png">Open ➜</a>',
-
-    backdrop: `
-        rgba(13, 13, 13, 0.92)
-      `,
+    confirmButtonText: '<a href="#">🔒 Upcoming</a>',
   });
 });
