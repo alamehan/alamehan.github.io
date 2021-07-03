eada0a136f82ccd558cd670326daca08be007a2d
alamehan <raifalam98@gmail.com>
2021-07-03 18:20:27 +0700

    UPDATE news

---
+++ news/pages/book/tab2-book.js
@@ -0,0 +1,47 @@
+$(document).ready(function () {
+
+  $('#100-in-history').DataTable({
+    'bPaginate': false, // Hide paginate
+    "bLengthChange": false,
+    "bInfo": false, // Hide entry text
+    "bAutoWidth": false,
+    "bFilter": false, // Hide search box
+    'columnDefs': [{
+      'orderable': false,
+      'targets': [0]
+    }], // Hide sort icon on header of first column
+    'aaSorting': [
+      [3, 'asc']
+    ], // Start to sort data in fourth column
+  });
+
+  $('#behind-the-scene').DataTable({
+    'bPaginate': false,
+    "bLengthChange": false,
+    "bInfo": false,
+    "bAutoWidth": false,
+    "bFilter": false,
+    'columnDefs': [{
+      'orderable': false,
+      'targets': [0]
+    }],
+    'aaSorting': [
+      [3, 'asc']
+    ],
+  });
+
+  $('#big-ideas-simply-explained').DataTable({
+    'bPaginate': false,
+    "bLengthChange": false,
+    "bInfo": false,
+    "bAutoWidth": false,
+    "bFilter": false,
+    'columnDefs': [{
+      'orderable': false,
+      'targets': [0]
+    }],
+    'aaSorting': [
+      [3, 'asc']
+    ],
+  });
+});
