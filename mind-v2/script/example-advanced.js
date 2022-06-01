6e88728bd8a1f76965b34737a26828469eb5e2f8
alamehan <raifalam98@gmail.com>
2022-06-01 09:24:11 +0700

    UPDATE ATOZ MIND V2

---
+++ mind-v2/script/example-advanced.js
@@ -0,0 +1,58 @@
+const { ref } = Vue;
+
+const app = Vue.createApp({
+  setup() {
+    const filter = ref('');
+    const filterRef = ref(null);
+
+    return {
+      filter,
+      filterRef,
+
+      splitterModel: ref(50),
+      selected: ref('Root'),
+
+      simple: [
+        {
+          label: 'Root',
+          avatar: 'https://cdn.quasar.dev/img/boy-avatar.png',
+          children: [
+            {
+              label: 'Good food (with icon)',
+              icon: 'restaurant_menu',
+              children: [
+                { label: 'Quality ingredients' },
+                { label: 'Good recipe' }]
+            },
+            {
+              label: 'Good service (disabled node with icon)',
+              icon: 'room_service',
+              disabled: true,
+              children: [
+                { label: 'Prompt attention' },
+                { label: 'Professional waiter' }]
+            },
+            {
+              label: 'Pleasant surroundings (with icon)',
+              icon: 'photo',
+              children: [
+                {
+                  label: 'Happy atmosphere (with image)',
+                  img: 'https://cdn.quasar.dev/img/logo_calendar_128px.png'
+                },
+                { label: 'Good table presentation' },
+                { label: 'Pleasing decor' }]
+            }]
+        }
+      ],
+
+      resetFilter() {
+        filter.value = '';
+        filterRef.value.focus();
+      }
+    };
+  }
+});
+
+app.use(Quasar, { config: {} });
+app.mount('#q-app');
